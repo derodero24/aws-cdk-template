7d7442404d9979e497b83a53d117736a059cba7c
derodero24 <k.itsh20524abc@gmail.com>
2022-10-04 19:32:29 +0900

    update packages

--- bin/aws-cdk-template.ts
+++ bin/aws-cdk-template.ts
@@ -5,6 +5,7 @@ import * as cdk from 'aws-cdk-lib';
 import { AwsCdkTemplateStack } from '../lib/aws-cdk-template-stack';
 
 const app = new cdk.App();
+
 new AwsCdkTemplateStack(app, 'AwsCdkTemplateStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
